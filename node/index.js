d4c191494678ff6cdb07fd534bcfd8e16a51212e
Ubuntu <ubuntu@ip-192-168-10-200.ap-south-1.compute.internal>
2024-06-01 09:00:02 +0000

    made timesheet reminders

--- node/index.js
+++ node/index.js
@@ -102,7 +102,7 @@ async function sendLogInReminder() {
               <h2>Reminder: Log in to AutomHR</h2>
             </div>
             <div class="email-body">
-              <p>Dear ${user.firstname},</p>
+              <p>Dear ${user?.firstname},</p>
               <p>This is a friendly reminder to log in to <strong>AutomHR</strong> to complete your timesheet and update any pending tasks.</p>
               <p>Please make sure to log in at your earliest convenience.</p>
               <a href="https://marx.automhr.com/admin/" class="email-button">Log in to AutomHR</a>
@@ -114,7 +114,7 @@ async function sendLogInReminder() {
         </body>
         </html>
       `;
-      await sendEmail(user.email, subject, html);
+      // await sendEmail(user.email, subject, html);
     }
   } catch (error) {
     console.error("Error sending login reminders:", error);
@@ -124,36 +124,49 @@ async function sendLogInReminder() {
 }
 
 async function checkTimesheets() {
+  const currentDate = moment();
+  let currentMonth;
+
+  if (currentDate.date() > currentDate.daysInMonth() - 2) {
+    currentMonth = currentDate.format("YYYY-MM");
+  } else if (currentDate.date() <= 2) {
+    currentMonth = currentDate.subtract(1, 'month').format("YYYY-MM");
+  } else {
+    console.log('Not within the specified date range for timesheet checking.');
+    return;
+  }
+  console.log("Checking timesheets for:", currentMonth);
+
   const connection = await mysql.createConnection(dbConfig);
 
   try {
-    const [users] = await connection.execute("SELECT * FROM tblstaff");
-    const currentMonth = moment().format("YYYY-MM");
-    console.log(users[0]);
-    for (let user of users) {
-      const [projects] = await connection.execute(
-        "SELECT * FROM tblproject_members WHERE staff_id = ?",
-        [user.staffid]
-      );
+    const [users] = await connection.execute("SELECT * FROM tblstaff WHERE status_work = ?",["working"]);
+    // const [users] = await connection.execute("SELECT * FROM tblstaff WHERE status_work = ? AND staffid = ?",["working",67]);
 
-      for (let project of projects) {
-        const [tasks] = await connection.execute(
-          "SELECT t.* FROM tbltasks t " +
-            "INNER JOIN tbltask_assigned ta ON t.id = ta.task_id " +
-            "WHERE t.rel_id = ? AND t.rel_type = ? AND ta.staffid = ?",
-          [project.id, "project", user.staffid]
-        );
+    await Promise.all(users.map(async (user) => {
+      const [projects] = await connection.execute(`
+        SELECT pm.*, p.name AS project_name
+        FROM tblproject_members pm
+        INNER JOIN tblprojects p ON pm.project_id = p.id
+        WHERE pm.staff_id = ?
+      `, [user.staffid]);
+
+      await Promise.all(projects.map(async (project) => {
+        const [tasks] = await connection.execute(`
+          SELECT t.*, ta.staffid AS assigned_staff_id
+          FROM tbltasks t
+          INNER JOIN tbltask_assigned ta ON t.id = ta.taskid
+          WHERE t.rel_id = ? AND t.rel_type = ? AND ta.staffid = ?
+        `, [project.project_id, "project", user.staffid]);
 
         let hasActiveTimer = false;
-
         for (let task of tasks) {
-          const [taskTimers] = await connection.execute(
-            `SELECT * FROM tbltaskstimers 
-                WHERE task_id = ? 
-                AND DATE_FORMAT(FROM_UNIXTIME(start_time), '%Y-%m') = ?
-                `,
-            [task.id, currentMonth]
-          );
+          const [taskTimers] = await connection.execute(`
+            SELECT 1
+            FROM tbltaskstimers
+            WHERE task_id = ? AND DATE_FORMAT(FROM_UNIXTIME(start_time), '%Y-%m') = ?
+            LIMIT 1
+          `, [task.id, currentMonth]);
 
           if (taskTimers.length > 0) {
             hasActiveTimer = true;
@@ -162,40 +175,91 @@ async function checkTimesheets() {
         }
 
         if (tasks.length > 0 && hasActiveTimer) {
-          const [timesheet] = await connection.execute(
-            `SELECT * FROM tbltime_sheet_approval WHERE staff_id = ? AND project_ids = ? AND DATE_FORMAT(created_at, '%Y-%m') = ? AND status != 2`,
-            [user.id, project.id, currentMonth]
-          );
+          const [timesheet] = await connection.execute(`
+            SELECT 1
+            FROM tbltime_sheet_approval
+            WHERE staff_id = ? AND project_ids = ? AND DATE_FORMAT(created_at, '%Y-%m') = ? AND status != "2" AND teamlead_status != "2"
+            LIMIT 1
+          `, [user.staffid, project.project_id, currentMonth]);
 
           if (timesheet.length === 0) {
-            console.log(
-              `Timesheet missing for user ${user.firstname} ${user.lastname} on project ${project.name} for the month of ${currentMonth}`
-            );
-            // await sendEmail(
-            //   user.email,
-            //   "Timesheet Missing",
-            //   `Please submit your timesheet for the project ${project.name} for the month of ${currentMonth}.`
-            // );
+            const subject = `Missing Timesheet for the project ${project.project_name} for the month of ${currentDate.format("MMMM, YYYY")}`;
+            const html = `
+              <html>
+              <head>
+                <style>
+                  .email-container {
+                    font-family: Arial, sans-serif;
+                    line-height: 1.6;
+                    color: #333;
+                  }
+                  .email-header {
+                    background-color: #f7f7f7;
+                    padding: 10px;
+                    border-bottom: 1px solid #ddd;
+                  }
+                  .email-body {
+                    padding: 20px;
+                  }
+                  .email-footer {
+                    background-color: #f7f7f7;
+                    padding: 10px;
+                    border-top: 1px solid #ddd;
+                    text-align: center;
+                    font-size: 0.9em;
+                  }
+                  .email-button {
+                    display: inline-block;
+                    padding: 10px 15px;
+                    margin-top: 10px;
+                    color: #fff;
+                    background-color: #007bff;
+                    text-decoration: none;
+                    border-radius: 5px;
+                  }
+                </style>
+              </head>
+
+              <body>
+                <div class="email-container">
+                  <div class="email-header">
+                    <h2>Timesheet Submission Reminder</h2>
+                  </div>
+                  <div class="email-body">
+                    <p>Dear ${user?.firstname},</p>
+                    <p>This is a friendly reminder to submit your Timesheet for the project <strong> ${project.project_name} </strong> for the month of <strong>${currentDate.format("MMMM, YYYY")}</strong> on AutomHR.</p>
+                    <p>Kindly follow the link below to submit the timesheet.</p>
+                    <p>Please ignore if all timesheets are submitted and approved.</p>
+                    <a href="https://marx.automhr.com/admin/staff/timesheets?view=all class="email-button">Submit Timesheet</a>
+                  </div>
+                  <div class="email-footer">
+                    <p>Best regards,<br>Marx HR</p>
+                  </div>
+                </div>
+              </body>
+              </html>
+            `;
+            await sendEmail(user.email, subject, html);
           }
         }
-      }
-    }
+      }));
+    }));
   } catch (error) {
     console.error("Error checking timesheets:", error);
   } finally {
-    // await connection.end();
+    await connection.end();
   }
 }
 
-// checkTimesheets();
-cron.schedule("00 12 * * *", async () => {
-  const today = moment().format("YYYY-MM-DD");
-  const start = moment("2024-05-03");
-  const end = moment("2024-06-07");
+// cron.schedule("00 12 * * *", async () => {
+//   const today = moment().format("YYYY-MM-DD");
+//   const start = moment("2024-05-03");
+//   const end = moment("2024-06-07");
 
-  if (moment(today).isBetween(start, end, "days", "[]")) {
-    await sendLogInReminder();
-  }
-});
+//   if (moment(today).isBetween(start, end, "days", "[]")) {
+//     await sendLogInReminder();
+//   }
+// });
 
+// cron.schedule('0 12 * * *', checkTimesheets);
 console.log("Scheduler is running...");
